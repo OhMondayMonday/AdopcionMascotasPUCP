c03477f8e0675a84c3dfd3144251920bac603313
1JuanDaniel0 <a20206452@pucp.edu.pe>
2024-11-03 17:20:40 -0500

    login y registro
    falta algunas cosas

--- HTML_Iweb/assets/js/app-user-view-security.js
+++ HTML_Iweb/assets/js/app-user-view-security.js
@@ -1,1 +1,51 @@
-"use strict";!function(){const e=document.querySelector("#formChangePassword");e&&FormValidation.formValidation(e,{fields:{newPassword:{validators:{notEmpty:{message:"Por favor ingresa una nueva contraseña"},stringLength:{min:8,message:"La contraseña debe contener más de 8 caracteres"}}},confirmPassword:{validators:{notEmpty:{message:"Por favor confirma tu contraseña"},identical:{compare:function(){return e.querySelector('[name="newPassword"]').value},message:"Las contraseñas no son iguales"},stringLength:{min:8,message:"La contraseña debe tener más de 8 caracteres"}}}},plugins:{trigger:new FormValidation.plugins.Trigger,bootstrap5:new FormValidation.plugins.Bootstrap5({eleValidClass:"",rowSelector:".form-password-toggle"}),submitButton:new FormValidation.plugins.SubmitButton,autoFocus:new FormValidation.plugins.AutoFocus},init:e=>{e.on("plugins.message.placed",function(e){e.element.parentElement.classList.contains("input-group")&&e.element.parentElement.insertAdjacentElement("afterend",e.messageElement)})}})}();
+"use strict";
+!(function () {
+  const e = document.querySelector("#formChangePassword");
+  e &&
+    FormValidation.formValidation(e, {
+      fields: {
+        newPassword: {
+          validators: {
+            notEmpty: { message: "Por favor ingresa una nueva contraseña" },
+            stringLength: {
+              min: 8,
+              message: "La contraseña debe contener más de 8 caracteres",
+            },
+          },
+        },
+        confirmPassword: {
+          validators: {
+            notEmpty: { message: "Por favor confirma tu contraseña" },
+            identical: {
+              compare: function () {
+                return e.querySelector('[name="newPassword"]').value;
+              },
+              message: "Las contraseñas no son iguales",
+            },
+            stringLength: {
+              min: 8,
+              message: "La contraseña debe tener más de 8 caracteres",
+            },
+          },
+        },
+      },
+      plugins: {
+        trigger: new FormValidation.plugins.Trigger(),
+        bootstrap5: new FormValidation.plugins.Bootstrap5({
+          eleValidClass: "",
+          rowSelector: ".form-password-toggle",
+        }),
+        submitButton: new FormValidation.plugins.SubmitButton(),
+        autoFocus: new FormValidation.plugins.AutoFocus(),
+      },
+      init: (e) => {
+        e.on("plugins.message.placed", function (e) {
+          e.element.parentElement.classList.contains("input-group") &&
+            e.element.parentElement.insertAdjacentElement(
+              "afterend",
+              e.messageElement
+            );
+        });
+      },
+    });
+})();
--- HTML_Iweb/assets/js/pages-auth-multisteps.js
+++ HTML_Iweb/assets/js/pages-auth-multisteps.js
@@ -1,1 +1,231 @@
-"use strict";$(function(){var e=$(".select2");e.length&&e.each(function(){var e=$(this);e.wrap('<div class="position-relative"></div>'),e.select2({placeholder:"Select an country",dropdownParent:e.parent()})})}),document.addEventListener("DOMContentLoaded",function(e){var n=document.querySelector("#multiStepsValidation");if(null!==n){var a=n.querySelector("#multiStepsForm");const c=a.querySelector("#accountDetailsValidation");var i=a.querySelector("#personalInfoValidation"),s=a.querySelector("#billingLinksValidation"),r=[].slice.call(a.querySelectorAll(".btn-next")),a=[].slice.call(a.querySelectorAll(".btn-prev")),o=document.querySelector(".multi-steps-exp-date"),l=document.querySelector(".multi-steps-cvv"),m=document.querySelector(".multi-steps-mobile"),u=document.querySelector(".multi-steps-pincode"),d=document.querySelector(".multi-steps-card");o&&new Cleave(o,{date:!0,delimiter:"/",datePattern:["m","y"]}),l&&new Cleave(l,{numeral:!0,numeralPositiveOnly:!0}),m&&new Cleave(m,{phone:!0,phoneRegionCode:"US"}),u&&new Cleave(u,{delimiter:"",numeral:!0}),d&&new Cleave(d,{creditCard:!0,onCreditCardTypeChanged:function(e){document.querySelector(".card-type").innerHTML=""!=e&&"unknown"!=e?'<img src="'+assetsPath+"img/icons/payments/"+e+'-cc.png" height="28"/>':""}});let t=new Stepper(n,{linear:!0});const p=FormValidation.formValidation(c,{fields:{multiStepsUsername:{validators:{notEmpty:{message:"Please enter username"},stringLength:{min:6,max:30,message:"The name must be more than 6 and less than 30 characters long"},regexp:{regexp:/^[a-zA-Z0-9 ]+$/,message:"The name can only consist of alphabetical, number and space"}}},multiStepsEmail:{validators:{notEmpty:{message:"Please enter email address"},emailAddress:{message:"The value is not a valid email address"}}},multiStepsPass:{validators:{notEmpty:{message:"Please enter password"}}},multiStepsConfirmPass:{validators:{notEmpty:{message:"Confirm Password is required"},identical:{compare:function(){return c.querySelector('[name="multiStepsPass"]').value},message:"The password and its confirm are not the same"}}}},plugins:{trigger:new FormValidation.plugins.Trigger,bootstrap5:new FormValidation.plugins.Bootstrap5({eleValidClass:"",rowSelector:".col-sm-6"}),autoFocus:new FormValidation.plugins.AutoFocus,submitButton:new FormValidation.plugins.SubmitButton},init:e=>{e.on("plugins.message.placed",function(e){e.element.parentElement.classList.contains("input-group")&&e.element.parentElement.insertAdjacentElement("afterend",e.messageElement)})}}).on("core.form.valid",function(){t.next()}),g=FormValidation.formValidation(i,{fields:{multiStepsFirstName:{validators:{notEmpty:{message:"Please enter first name"}}},multiStepsAddress:{validators:{notEmpty:{message:"Please enter your address"}}}},plugins:{trigger:new FormValidation.plugins.Trigger,bootstrap5:new FormValidation.plugins.Bootstrap5({eleValidClass:"",rowSelector:function(e,t){switch(e){case"multiStepsFirstName":return".col-sm-6";case"multiStepsAddress":return".col-md-12";default:return".row"}}}),autoFocus:new FormValidation.plugins.AutoFocus,submitButton:new FormValidation.plugins.SubmitButton}}).on("core.form.valid",function(){t.next()}),v=FormValidation.formValidation(s,{fields:{multiStepsCard:{validators:{notEmpty:{message:"Please enter card number"}}}},plugins:{trigger:new FormValidation.plugins.Trigger,bootstrap5:new FormValidation.plugins.Bootstrap5({eleValidClass:"",rowSelector:function(e,t){return"multiStepsCard"!==e?".col-dm-6":".col-md-12"}}),autoFocus:new FormValidation.plugins.AutoFocus,submitButton:new FormValidation.plugins.SubmitButton},init:e=>{e.on("plugins.message.placed",function(e){e.element.parentElement.classList.contains("input-group")&&e.element.parentElement.insertAdjacentElement("afterend",e.messageElement)})}}).on("core.form.valid",function(){alert("Submitted..!!")});r.forEach(e=>{e.addEventListener("click",e=>{switch(t._currentIndex){case 0:p.validate();break;case 1:g.validate();break;case 2:v.validate()}})}),a.forEach(e=>{e.addEventListener("click",e=>{switch(t._currentIndex){case 2:case 1:t.previous()}})})}});
+"use strict";
+$(function () {
+  var e = $(".select2");
+  e.length &&
+    e.each(function () {
+      var e = $(this);
+      e.wrap('<div class="position-relative"></div>'),
+        e.select2({
+          placeholder: "Select an country",
+          dropdownParent: e.parent(),
+        });
+    });
+}),
+  document.addEventListener("DOMContentLoaded", function (e) {
+    var n = document.querySelector("#multiStepsValidation");
+    if (null !== n) {
+      var a = n.querySelector("#multiStepsForm");
+      const c = a.querySelector("#accountDetailsValidation");
+      var i = a.querySelector("#personalInfoValidation"),
+        s = a.querySelector("#billingLinksValidation"),
+        r = [].slice.call(a.querySelectorAll(".btn-next")),
+        a = [].slice.call(a.querySelectorAll(".btn-prev")),
+        o = document.querySelector(".multi-steps-exp-date"),
+        l = document.querySelector(".multi-steps-cvv"),
+        m = document.querySelector(".multi-steps-mobile"),
+        u = document.querySelector(".multi-steps-pincode"),
+        d = document.querySelector(".multi-steps-card");
+      o && new Cleave(o, { date: !0, delimiter: "/", datePattern: ["m", "y"] }),
+        l && new Cleave(l, { numeral: !0, numeralPositiveOnly: !0 }),
+        m && new Cleave(m, { phone: !0, phoneRegionCode: "US" }),
+        u && new Cleave(u, { delimiter: "", numeral: !0 }),
+        d &&
+          new Cleave(d, {
+            creditCard: !0,
+            onCreditCardTypeChanged: function (e) {
+              document.querySelector(".card-type").innerHTML =
+                "" != e && "unknown" != e
+                  ? '<img src="' +
+                    assetsPath +
+                    "img/icons/payments/" +
+                    e +
+                    '-cc.png" height="28"/>'
+                  : "";
+            },
+          });
+      let t = new Stepper(n, { linear: !0 });
+      const p = FormValidation.formValidation(c, {
+          fields: {
+            multiStepsUsername: {
+              validators: {
+                notEmpty: { message: "Por favor ingresa tu nombre de usuario" },
+                stringLength: {
+                  min: 6,
+                  max: 30,
+                  message:
+                    "El nombre debe tener entre 6 a 30 caracteres",
+                },
+                regexp: {
+                  regexp: /^[a-zA-Z0-9._-]+$/,
+                  message: "El nombre de usuario solo puede contener letras, números, puntos, guiones y guiones bajos",
+                },
+              },
+            },
+            multiStepsEmail: {
+              validators: {
+                notEmpty: { message: "Por favor ingresa un correo electrónico" },
+                emailAddress: {
+                  message: "El correo ingresado no es válido",
+                },
+              },
+            },
+            multiStepsPass: {
+              validators: { notEmpty: { message: "Por favor ingresa una contraseña" } },
+            },
+            multiStepsConfirmPass: {
+              validators: {
+                notEmpty: { message: "Debes confirmar tu contraseña" },
+                identical: {
+                  compare: function () {
+                    return c.querySelector('[name="multiStepsPass"]').value;
+                  },
+                  message: "Las contraseñas no coinciden",
+                },
+              },
+            },
+          },
+          plugins: {
+            trigger: new FormValidation.plugins.Trigger(),
+            bootstrap5: new FormValidation.plugins.Bootstrap5({
+              eleValidClass: "",
+              rowSelector: ".col-sm-6",
+            }),
+            autoFocus: new FormValidation.plugins.AutoFocus(),
+            submitButton: new FormValidation.plugins.SubmitButton(),
+          },
+          init: (e) => {
+            e.on("plugins.message.placed", function (e) {
+              e.element.parentElement.classList.contains("input-group") &&
+                e.element.parentElement.insertAdjacentElement(
+                  "afterend",
+                  e.messageElement
+                );
+            });
+          },
+        }).on("core.form.valid", function () {
+          t.next();
+        }),
+        g = FormValidation.formValidation(i, {
+          fields: {
+            multiStepsFirstName: {
+              validators: { 
+                notEmpty: { message: "Por favor ingresa tu nombre" },
+                regexp: {
+                    regexp: /^[a-zA-Z\s]+$/,
+                    message: "El nombre sólo puede contener letras",
+                },
+              },
+            },
+            multiStepsLastName: {
+                validators: {
+                  notEmpty: { message: "Por favor ingresa tu apellido" },
+                  regexp: {
+                    regexp: /^[a-zA-Z\s]+$/,
+                    message: "El apellido solo puede contener letras",
+                  },
+                },
+            },
+            multiStepsAddress: {
+              validators: {
+                notEmpty: { message: "Por favor ingresa tu dirección" },
+              },
+            },
+          },
+          plugins: {
+            trigger: new FormValidation.plugins.Trigger(),
+            bootstrap5: new FormValidation.plugins.Bootstrap5({
+              eleValidClass: "",
+              rowSelector: function (e, t) {
+                switch (e) {
+                  case "multiStepsFirstName":
+                    return ".col-sm-6";
+                  case "multiStepsAddress":
+                    return ".col-md-12";
+                  default:
+                    return ".row";
+                }
+              },
+            }),
+            autoFocus: new FormValidation.plugins.AutoFocus(),
+            submitButton: new FormValidation.plugins.SubmitButton(),
+          },
+        }).on("core.form.valid", function () {
+            // Muestra el popup de SweetAlert2
+            Swal.fire({
+                title: '¡Solicitud enviada!',
+                text: 'Tus datos han sido enviados y serán revisados por un administrador. Te enviaremos un correo de confirmación lo más pronto posible.',
+                icon: 'success',
+                confirmButtonText: 'Aceptar',
+                customClass: {
+                confirmButton: 'btn btn-primary'
+                },
+                buttonsStyling: false
+            }).then((result) => {
+              if (result.isConfirmed) {
+                  window.location.href = '../../index.html';
+              }
+          });
+        });
+      r.forEach((e) => {
+        e.addEventListener("click", (e) => {
+          switch (t._currentIndex) {
+            case 0:
+              p.validate();
+              break;
+            case 1:
+              g.validate();
+              break;
+            case 2:
+              v.validate();
+          }
+        });
+      }),
+        a.forEach((e) => {
+          e.addEventListener("click", (e) => {
+            switch (t._currentIndex) {
+              case 2:
+              case 1:
+                t.previous();
+            }
+          });
+        });
+    }
+  });
+  document.addEventListener("DOMContentLoaded", function () {
+    const documentType = document.getElementById("documentType");
+    const documentNumber = document.getElementById("multiStepsMobile");
+  
+    // Deshabilitar el campo de documento inicialmente
+    //documentNumber.disabled = true;
+  
+    function updateDocumentValidation() {
+      const selectedType = documentType.value;
+  
+      if (selectedType === "DNI") {
+        documentNumber.disabled = false; // Habilitar el campo
+        documentNumber.setAttribute("maxlength", "8");
+        documentNumber.setAttribute("pattern", "^\\d{8}$");
+        documentNumber.placeholder = "Ingrese su número DNI";
+      } else if (selectedType === "CARNET_EXT") {
+        documentNumber.disabled = false; // Habilitar el campo
+        documentNumber.setAttribute("maxlength", "12");
+        documentNumber.setAttribute("pattern", "^\\d{11,12}$");
+        documentNumber.placeholder = "Ingrese número de CE";
+      } else {
+        documentNumber.disabled = true; // Deshabilitar si no se selecciona tipo
+        documentNumber.value = ""; // Limpia el campo si se vuelve a "Tipo"
+        documentNumber.removeAttribute("maxlength");
+        documentNumber.removeAttribute("pattern");
+        documentNumber.placeholder = "Ingresa tu documento";
+      }
+    }
+  
+    // Permitir solo números en el campo de documento
+    documentNumber.addEventListener("input", function (event) {
+      documentNumber.value = documentNumber.value.replace(/\D/g, ""); // Elimina cualquier caracter no numérico
+    });
+  
+    // Actualizar la validación cuando cambia el tipo de documento
+    documentType.addEventListener("change", updateDocumentValidation);
+  });
+  
