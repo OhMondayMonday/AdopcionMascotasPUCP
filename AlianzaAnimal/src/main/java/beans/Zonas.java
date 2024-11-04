b8d9f9b6e849ba1717dcedc7a37e51a58ab52061
OhMondayMonday <a20216352@pucp.edu.pe>
2024-11-04 10:53:21 -0500

    beans avance

---
+++ AlianzaAnimal/src/main/java/beans/Zonas.java
@@ -0,0 +1,21 @@
+package beans;
+public class Zonas {
+    private int zonaId;
+    private String nombreZona;
+
+    public int getZonaId() {
+        return zonaId;
+    }
+
+    public void setZonaId(int zonaId) {
+        this.zonaId = zonaId;
+    }
+
+    public String getNombreZona() {
+        return nombreZona;
+    }
+
+    public void setNombreZona(String nombreZona) {
+        this.nombreZona = nombreZona;
+    }
+}
